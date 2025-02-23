7f9dfa5bfcb6a9c02d038814fed064ad997649bb
Allan Francis <allantecnologo@gmail.com>
2025-02-23 00:05:20 -0400

    Add Prettier configuration, ESLint plugin for import sorting, and update button component styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
       FSW MCDonalds
     </main>
   );
