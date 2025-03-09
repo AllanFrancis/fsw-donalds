fbeb5c65ede2c475ed5c2ddd0b8292f1d6a24fbc
Allan Francis <allantecnologo@gmail.com>
2025-03-09 15:02:09 -0400

    Implement restaurant retrieval by slug, add unique constraint to slug field, and create restaurant menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,37 @@
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/app/data/get-restaurant-by-slug";
+
+import RestarantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethod = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethod(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await getRestaurantBySlug(slug);
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <RestarantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
