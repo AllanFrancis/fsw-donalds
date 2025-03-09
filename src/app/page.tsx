275d1b5bb5100d150ed0bb370ba2e00324757cd9
Allan Francis <allantecnologo@gmail.com>
2025-03-09 13:02:56 -0400

    Remove unused SVG files and add Order Now button to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
       FSW MCDonalds
+      <Button>Order Now</Button>
     </main>
   );
 }
